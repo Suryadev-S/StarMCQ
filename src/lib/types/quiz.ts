06d0240ede3cf0586c2cb24bbaeb74e46578bd7d
suryadev <somasuryadev6@gmail.com>
2025-08-26 13:16:58 +0530

    added quiz store and quiz type

---
+++ src/lib/types/quiz.ts
@@ -0,0 +1,9 @@
+export interface IQuiz {
+    quizId: string;
+    question: string;
+    options: {
+        optionId: string;
+        optionContent: string;
+    }[];
+    correctOption: string;
+}[]
